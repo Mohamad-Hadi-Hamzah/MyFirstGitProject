700faa10308fce1a48dcb85509caf52edb21287c
Mohamad-Hadi-Hamzah <mymimpistudio@gmail.com>
2025-10-16 10:28:00 +0800

    Ex2

---
+++ ex1.py
@@ -0,0 +1,7 @@
+def checkOddEven(number):
+    if(number%2==0):
+        print(f'{number} is Even Number')
+    else:print(f'{number} is Odd Number')
+
+givenNumber=int(input('Enter Number: '))
+checkOddEven(givenNumber)
